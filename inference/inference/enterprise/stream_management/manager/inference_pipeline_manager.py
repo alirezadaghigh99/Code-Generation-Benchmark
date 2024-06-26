d0f19c5c1fd6fd2220355bae27c0c1a7b45f74b3
alireza <Alireza.daghigh1999@gmail.com>
2024-06-26 10:41:45 -0400

    Updated init in /home/aliredaq/Desktop/Code-Generation-Benchmark/inference/inference/enterprise/stream_management/manager/inference_pipeline_manager.py

---
+++ inference/inference/enterprise/stream_management/manager/inference_pipeline_manager.py
@@ -0,0 +1,4 @@
+    def init(
+        cls, command_queue: Queue, responses_queue: Queue
+    ) -> "InferencePipelineManager":
+        return cls(command_queue=command_queue, responses_queue=responses_queue)
